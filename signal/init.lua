70f8cef782c3de90c2635fa240af3c0702ea3f87
JuanZoran <1430359574@qq.com>
2023-06-04 10:58:17 +0800

    chore: init commit

---
+++ signal/init.lua
@@ -0,0 +1,40 @@
+require 'signal.battery'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- local path = ... .. '.'
+-- return setmetatable({}, {
+--     __index = function(self, key)
+--         local v = require(path .. key)
+--         rawset(self, key, v)
+--         return v
+--     end,
+-- })
---
+++ ui/wibar/systray.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        {
+            widget = wibox.widget.systray,
+            base_size = 25,
+        },
+        shape  = gears.shape.rounded_rect,
+        widget = wibox.container.background,
+    },
+    left = 8,
+    right = 8,
+    top = 8,
+    widget = wibox.container.margin,
+}
